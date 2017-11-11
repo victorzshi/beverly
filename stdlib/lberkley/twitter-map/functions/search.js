628240c58508dee4d2565cf49eeb03790bb58028
Lincoln Berkley <linc.berkley@yale.edu>
2017-11-11 00:41:49 -0500

    Filter search to geotagged tweets, and extract lat and lon

--- stdlib/lberkley/twitter-map/functions/search.js
+++ stdlib/lberkley/twitter-map/functions/search.js
@@ -3,7 +3,7 @@ var Twitter = require('twitter');
 /**
 * Searches Twitter for a given term
 * @param {string} term What you're searching for
-* @returns {string}
+* @returns {array}
 */
 module.exports = (term = "HackPrinceton", context, callback) => {
 
@@ -14,8 +14,36 @@ module.exports = (term = "HackPrinceton", context, callback) => {
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   });
 
-  client.get('search/tweets', {q: term}, function(error, tweets, response) {
-    callback(null, tweets.statuses[0]);
+  req_obj = {
+    q: term,
+    geocode: "40,-75,10mi",
+    count: 100
+  }
+
+  client.get('search/tweets', req_obj, function(error, tweets, response) {
+    tweet_data = [];
+    for (tid in tweets.statuses) {
+      t = tweets.statuses[tid];
+      if (t.coordinates != null) {
+        tweet_data.push({
+          text: t.text,
+          lat: t.coordinates.coordinates[0],
+          lon: t.coordinates.coordinates[1],
+          source: "coordinates"
+        });
+      } else if (t.place != null) {
+        box = t.place.bounding_box.coordinates[0]
+        lat = (box[0][0] + box[1][0]) / 2
+        lon = (box[1][1] + box[2][1]) / 2
+        tweet_data.push({
+          text: t.text,
+          lat: lat,
+          lon: lon,
+          source: "place"
+        });
+      }
+    }
+    callback(null, tweet_data);
   });
 
 };
