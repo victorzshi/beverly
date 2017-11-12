18c8a524519c1cba3284e75d164be6f414a4e654
VictoryShoe <victoryshoe@hotmail.com>
2017-11-12 00:55:33 -0500

    Display heatmap

--- frontend/src/components/HeatMap.vue
+++ frontend/src/components/HeatMap.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
     <span>Display child prop-passed message: {{ message }}</span>
+
+<!--     <div id="filters-panel">
+      <div class="field">
+        <a class="button" v-on:click="toggleHeatmap()">Toggle Heatmap</a>
+      </div>
+    </div> -->
+
     <div id="map"></div>
   </div>
 </template>
@@ -17,173 +24,62 @@
 <script>
 export default {
   name: 'HeatMap',
-  props: ['mapCenter', 'heatMapData', 'message'],
+  props: ['mapCenter', 'heatmapData', 'message'],
   data: function () {
     return {
-      map: null,
-      heatmap: null,
-      // Possibly pass an array of points, or grab it
-      // markerCoordinates: [{
-      //   latitude: 51.501527,
-      //   longitude: -0.1921837
-      // }, {
-      //   latitude: 51.505874,
-      //   longitude: -0.1838486
-      // }, {
-      //   latitude: 51.4998973,
-      //   longitude: -0.202432
-      // }]
-    }
-  },
-  methods: {
-    initMap: function () {
-
+      markerCoordinates: [{
+        latitude: 51.501527,
+        longitude: -0.1921837
+      }, {
+        latitude: 51.505874,
+        longitude: -0.1838486
+      }, {
+        latitude: 51.4998973,
+        longitude: -0.202432
+      }],
     }
   },
   mounted: function () {
-    // Map settings
+    const element = document.getElementById('map')
     const options = {
-      center: new google.maps.LatLng(this.mapCenter.lat,this.mapCenter.lng),
-      mapTypeControl: false,
-      fullscreenControl: false,
-      zoom: 13,
-      streetViewControl: false,
-      styles: [
-        {
-          "featureType": "administrative",
-          "elementType": "all",
-          "stylers": [
-            {
-              // "visibility": "off"
-              "visibility": "on"
-            }
-          ]
-        },
-        {
-          "featureType": "landscape",
-          "elementType": "all",
-          "stylers": [
-            {
-              "visibility": "simplified"
-            },
-            {
-              "hue": "#0066ff"
-            },
-            {
-              "saturation": 74
-            },
-            {
-              "lightness": 100
-            }
-          ]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "labels.icon",
-          "stylers": [
-            {
-              "visibility": "off"
-            }
-          ]
-        },
-        {
-          "featureType": "road",
-          "elementType": "all",
-          "stylers": [
-            {
-              // "visibility": "simplified"
-              "visibility": "on"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "all",
-          "stylers": [
-            {
-              "visibility": "off"
-            },
-            {
-              "weight": 0.6
-            },
-            {
-              "saturation": -85
-            },
-            {
-              "lightness": 61
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "visibility": "on"
-            }
-          ]
-        },
-        {
-          "featureType": "road.arterial",
-          "elementType": "all",
-          "stylers": [
-            {
-              // "visibility": "simplified"
-              "visibility": "on"
-            }
-          ]
-        },
-        {
-          "featureType": "road.local",
-          "elementType": "all",
-          "stylers": [
-            {
-              // "visibility": "simplified"
-              "visibility": "on"
-            }
-          ]
-        },
-        {
-          "featureType": "transit",
-          "elementType": "all",
-          "stylers": [
-            {
-              // "visibility": "simplified"
-              "visibility": "on"
-            }
-          ]
-        },
-        {
-          "featureType": "water",
-          "elementType": "all",
-          "stylers": [
-            {
-              "visibility": "simplified"
-            },
-            {
-              "color": "#5f94ff"
-            },
-            {
-              "lightness": 26
-            },
-            {
-              "gamma": 5.86
-            }
-          ]
-        }
-      ]
+      zoom: 14,
+      // London? Show markers
+      // center: new google.maps.LatLng(51.501527,-0.1921837)
+      // San Francisco show the heat map
+      center: new google.maps.LatLng(37.774546, -122.433523)
     }
+    const map = new google.maps.Map(element, options);
 
-    // Create Google map
-    this.map = new google.maps.Map(document.getElementById('map'), options);
-    // this.markerCoordinates.forEach((givenCoord) => {
-    //   const position = new google.maps.LatLng(givenCoord.latitude, givenCoord.longitude);
-    //   const marker = new google.maps.Marker({ 
-    //     position,
-    //     map
-    //   })
-    // })
-    
+    this.markerCoordinates.forEach((coord) => {
+      const position = new google.maps.LatLng(coord.latitude, coord.longitude);
+      const marker = new google.maps.Marker({ 
+        position,
+        map
+      });
+    });
+
+    let heatmapData = [
+      new google.maps.LatLng(37.782, -122.447),
+      new google.maps.LatLng(37.782, -122.445),
+      new google.maps.LatLng(37.782, -122.443),
+      new google.maps.LatLng(37.782, -122.441),
+      new google.maps.LatLng(37.782, -122.439),
+      new google.maps.LatLng(37.782, -122.437),
+      new google.maps.LatLng(37.782, -122.435),
+      new google.maps.LatLng(37.785, -122.447),
+      new google.maps.LatLng(37.785, -122.445),
+      new google.maps.LatLng(37.785, -122.443),
+      new google.maps.LatLng(37.785, -122.441),
+      new google.maps.LatLng(37.785, -122.439),
+      new google.maps.LatLng(37.785, -122.437),
+      new google.maps.LatLng(37.785, -122.435)
+    ];
+
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+      data: heatmapData
+    });
+    heatmap.setMap(map);
+
   }
 };
 </script>
